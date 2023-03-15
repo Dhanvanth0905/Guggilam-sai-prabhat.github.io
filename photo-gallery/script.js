baf228a4f8c263d52b8084ea1bb73859013d72fd
Prabhat0321 <guggliamprabhat@gmail.com>
2023-03-16 00:09:05 +0530

    modified photo-gallery

--- photo-gallery/script.js
+++ photo-gallery/script.js
@@ -1,32 +1,91 @@
-// Get the button and image elements
 const reddishButton = document.getElementById('reddish-btn');
-const blueishButton = document.getElementById('blueish-btn')
+const blueishButton = document.getElementById('blueish-btn');
+const duplicateButton = document.getElementById('duplicate-btn');
 const images = document.querySelectorAll('.gallery img');
 
-// Hide the reddish button initially
+// Hide the buttons initially
 reddishButton.style.display = 'none';
 blueishButton.style.display = 'none';
+duplicateButton.style.display = 'none';
 
-// Add a click event listener to each image element
+let clickedImage = null;
+
 images.forEach(img => {
-  img.addEventListener('rightclick', () => {
-    // Show the reddish button
+  img.addEventListener('click', (event) => {
+    // Show the buttons
     reddishButton.style.display = 'block';
     blueishButton.style.display = 'block';
-    
-  });
-  reddishButton.addEventListener('click', () => {
-    // Loop through each image element and apply the reddish filter class
-    images.forEach(img => {
-      img.classList.add('reddish');
-    });
-  });
-  blueishButton.addEventListener('click', () =>{
-    images.forEach(img => {
-        img.classList.add('blueish');
-      });
+    duplicateButton.style.display = 'block';
 
-  })
+    // Store the clicked image
+    clickedImage = event.target;
+  });
 });
+
+blueishButton.addEventListener('click', () => {
+  if (clickedImage !== null) {
+    // Remove the reddish filter class (if it exists)
+    clickedImage.classList.remove('reddish');
+    
+    // Apply the blueish filter class to the clicked image
+    clickedImage.classList.add('blueish');
+  }
+});
+
+reddishButton.addEventListener('click', () => {
+  if (clickedImage !== null) {
+    // Remove the blueish filter class (if it exists)
+    clickedImage.classList.remove('blueish');
+    
+    // Apply the reddish filter class to the clicked image
+    clickedImage.classList.add('reddish');
+  }
+});
+
+duplicateButton.addEventListener('click', () => {
+  if (clickedImage !== null) {
+    // Create a new image element
+    const newImage = document.createElement('img');
+
+    // Set the source and alt attributes of the new image to the clicked image's source and alt attributes
+    newImage.src = clickedImage.src;
+    newImage.alt = clickedImage.alt;
+
+    // Create a new div element to wrap the new image
+    const newDiv = document.createElement('div');
+
+    // Add a border style to the new div element
+    // newDiv.style.border = '2px solid black';
+    // newDiv.style.margin = '20px';
+    // newDiv.style.float = 'left';
+    // newDiv.style.width = '50%';
+    // newDiv.style.height = '50%';
+    // newDiv.style.objectFit = 'cover';
+
+    // Append the new image to the new div element
+    newDiv.appendChild(newImage);
+
+    // Append the new div element to the parent container of the img elements
+    const gallery = document.querySelector('.gallery');
+    gallery.appendChild(newDiv);
+  }
+});
+
+function increaseBrightness() {
+  var images = document.querySelectorAll('.gallery img');
+  for (var i = 0; i < images.length; i++) {
+    var brightness = parseFloat(images[i].style.filter.replace('brightness(', '').replace('%)', '')) || 100;
+    brightness += 10;
+    images[i].style.filter = 'brightness(' + brightness + '%)';
+  }
+}
+function decreaseBrightness() {
+  var images = document.querySelectorAll('.gallery img');
+  for (var i = 0; i < images.length; i++) {
+    var brightness = parseFloat(images[i].style.filter.replace('brightness(', '').replace('%)', '')) || 100;
+    brightness -= 10;
+    images[i].style.filter = 'brightness(' + brightness + '%)';
+  }
+}
 
 
